237341b8c3df0703b1db71a57996113945947b54
Saint-Jean Germila <germila@noukod.com>
2018-11-23 21:10:22 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO series(id, title, author_id,subgenre_id) values
+                    (1, "Vegetables", 01, 40),
+                    (2, "Technologies", 02, 41);
+
+INSERT INTO Characters(id, name, species, motto, series_id, author_id) values
+                                (1, "name1", "motto1", 34, 45),
+                                (2, "name2", "motto2" 89, 56),
+                                (3, "nam3", "motto3", 30, 44),
+                                (4, "name4", "motto4" 898, 55),
+                                (5, "name5", "motto5", 33, 40),
+                                (6, "name6", "motto6" 87, 51),
+                                (7, "name7", "motto7", 37, 46),
+                                (8, "name8", "motto8" 39, 50);
